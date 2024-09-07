11a35fc7b561e6c1d96b20894299f34e34d017cc
joaovictorjs <skai7ami@gmail.com>
2024-09-07 13:30:21 -0300

    add textbox extensions

---
+++ up.suporte/Extensions/TextBoxExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace up.suporte.Extensions
+{
+	public partial class TextBoxExtensions : TextBox
+	{
+		public static readonly DependencyProperty PlaceholderProperty = DependencyProperty.RegisterAttached(
+			"Placeholder",
+			typeof(string),
+			typeof(TextBoxExtensions));
+
+		public static readonly DependencyProperty LabelProperty = DependencyProperty.RegisterAttached(
+			"Label",
+			typeof(string),
+			typeof(TextBoxExtensions));
+
+		public static string GetPlaceholder(TextBox textBox)
+		{
+			return (string)textBox.GetValue(PlaceholderProperty);
+		}
+
+		public static void SetPlaceholder(TextBox textBox, string value)
+		{
+			textBox.SetValue(PlaceholderProperty, value);
+		}
+
+		public static string GetLabel(TextBox textBox)
+		{
+			return (string)textBox.GetValue(LabelProperty);
+		}
+
+		public static void SetLabel(TextBox textBox, string value)
+		{
+			textBox.SetValue(LabelProperty, value);
+		}
+	}
+}
