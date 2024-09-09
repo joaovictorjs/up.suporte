0d2e9466e12d100c2be489363c90da31d7bd678f
joaovictorjs <skai7ami@gmail.com>
2024-09-09 17:16:51 -0300

    using dependency injectio

--- up.suporte/App.xaml.cs
+++ up.suporte/App.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using up.suporte.ViewModels;
+using up.suporte.Views;
 using up.suporte.Windows;
 
 namespace up.suporte
@@ -9,15 +12,32 @@ namespace up.suporte
     /// </summary>
     public partial class App : Application
     {
+        private IHost _host;
+
         public App()
         {
             InitializeComponent();
+
+            IHostBuilder builder = new HostBuilder();
+
+            builder.ConfigureServices(services =>
+            {
+                services.AddTransient<LoginWindowViewModel>();
+                services.AddTransient<LoginViewModel>();
+
+                services.AddTransient<LoginWindow>(local => new LoginWindow() { DataContext = local.GetRequiredService<LoginWindowViewModel>() });
+                services.AddTransient<LoginView>(local => new LoginView() { DataContext = local.GetRequiredService<LoginViewModel>() });
+            });
+
+            _host = builder.Build();
         }
 
         protected override void OnStartup(StartupEventArgs e)
         {
-            LoginWindow loginWindow = new LoginWindow() { DataContext = new LoginWindowViewModel() };
-            loginWindow.Show();
+            _host.Start();
+
+            LoginWindow loginWindow = _host?.Services.GetService<LoginWindow>();
+            loginWindow?.Show();
 
             base.OnStartup(e);
         }
