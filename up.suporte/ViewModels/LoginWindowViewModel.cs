3051b205fcc1346453ab15b8c97509996203fc30
joaovictorjs <skai7ami@gmail.com>
2024-09-09 08:24:56 -0300

    add viewmodel for login window

---
+++ up.suporte/ViewModels/LoginWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace up.suporte.ViewModels
+{
+    public class LoginWindowViewModel : BaseViewModel
+    {
+        private BaseViewModel _currentViewModel;
+
+        public BaseViewModel CurrentViewModel { get { return _currentViewModel; } }
+
+        public LoginWindowViewModel()
+        {
+            _currentViewModel = new LoginViewModel();
+        }
+    }
+}
