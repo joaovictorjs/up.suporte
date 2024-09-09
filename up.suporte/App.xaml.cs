2dc393c48d5ec7b73566d7f0a06f620ca40f1bf9
joaovictorjs <skai7ami@gmail.com>
2024-09-09 08:35:46 -0300

    remove startupUri & init LoginWindow from code behind

--- up.suporte/App.xaml.cs
+++ up.suporte/App.xaml.cs
@@ -1,12 +1,26 @@
 ﻿using System.Windows;
+using up.suporte.ViewModels;
+using up.suporte.Windows;
 
 namespace up.suporte
 {
-	/// <summary>
-	/// Interaction logic for App.xaml
-	/// </summary>
-	public partial class App : Application
-	{
-	}
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            LoginWindow loginWindow = new LoginWindow() { DataContext = new LoginWindowViewModel() };
+            loginWindow.Show();
+
+            base.OnStartup(e);
+        }
+    }
 
 }
