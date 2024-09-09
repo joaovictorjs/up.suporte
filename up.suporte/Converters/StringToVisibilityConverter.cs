dded239f0b27dac585dbd9cecafc6f5e7a74e2b1
joaovictorjs <skai7ami@gmail.com>
2024-09-09 15:04:33 -0300

    create string to visibility converter

---
+++ up.suporte/Converters/StringToVisibilityConverter.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace up.suporte.Converters
+{
+    public class StringToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool invert = bool.TryParse((string)parameter, out bool result) ? result : false;
+
+            if (invert)
+            {
+                return String.IsNullOrEmpty((string)value) ? Visibility.Visible : Visibility.Collapsed;
+            }
+            else
+            {
+                return String.IsNullOrEmpty((string)value) ? Visibility.Collapsed : Visibility.Visible;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
