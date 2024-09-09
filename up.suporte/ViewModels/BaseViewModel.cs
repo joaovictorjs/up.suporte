f39060ad2e934d0418a00fa52811238609d12b6e
joaovictorjs <skai7ami@gmail.com>
2024-09-09 08:16:15 -0300

    create base viewmodel

---
+++ up.suporte/ViewModels/BaseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace up.suporte.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
