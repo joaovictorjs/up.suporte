63bd85c1f2a1f4f503f4369321623322534b2c7e
joaovictorjs <skai7ami@gmail.com>
2024-09-09 23:52:48 -0300

    add RelayCommand

---
+++ up.suporte/Commands/RelayCommand.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows.Input;
+
+namespace up.suporte.Commands
+{
+	public class RelayCommand : ICommand
+	{
+		private Action<Object> _execute;
+		private Func<Object, bool>? _canExecute;
+
+		public RelayCommand(Action<Object> execute, Func<Object, bool>? canExecute = null)
+		{
+			_execute = execute;
+			_canExecute = canExecute;
+		}
+
+		public event EventHandler? CanExecuteChanged
+		{
+			add => CommandManager.RequerySuggested += value;
+			remove => CommandManager.RequerySuggested -= value;
+		}
+
+		public bool CanExecute(object? parameter)
+		{
+			return _canExecute == null || _canExecute.Invoke(parameter ?? EventArgs.Empty);
+		}
+
+		public void Execute(object? parameter)
+		{
+			_execute?.Invoke(parameter ?? EventArgs.Empty);
+		}
+	}
+}
