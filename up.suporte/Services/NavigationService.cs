522320e12ed613111404df7138d99d09af409e9a
joaovictorjs <skai7ami@gmail.com>
2024-09-10 22:22:30 -0300

    service to handle navigation

---
+++ up.suporte/Services/NavigationService.cs
@@ -0,0 +1,27 @@
+﻿using up.suporte.Stores;
+using up.suporte.ViewModels;
+
+namespace up.suporte.Services
+{
+	public interface INavigationService
+	{
+		public void Navigate();
+	}
+
+	public class NavigationService<TViewModel> : INavigationService where TViewModel : BaseViewModel
+	{
+		private NavigationStore _navStore;
+		private Func<TViewModel> _navFactory;
+
+		public NavigationService(NavigationStore store, Func<TViewModel> factory)
+		{
+			_navStore = store;
+			_navFactory = factory;
+		}
+
+		public void Navigate()
+		{
+			_navStore.CurrentViewModel = _navFactory.Invoke();
+		}
+	}
+}
