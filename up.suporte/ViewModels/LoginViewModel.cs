c7c9480f451908b0b470f1f0418c93edb219a33b
joaovictorjs <skai7ami@gmail.com>
2024-09-10 22:24:40 -0300

    add command to navigate to config connection

--- up.suporte/ViewModels/LoginViewModel.cs
+++ up.suporte/ViewModels/LoginViewModel.cs
@@ -1,6 +1,25 @@
-﻿namespace up.suporte.ViewModels
+﻿using System.Windows.Input;
+using up.suporte.Commands;
+using up.suporte.Services;
+
+namespace up.suporte.ViewModels
 {
-    public class LoginViewModel : BaseViewModel
-    {
-    }
+	public class LoginViewModel : BaseViewModel
+	{
+		private readonly INavigationService _navService;
+
+		public ICommand GoToConfigureConnectionCommand
+		{
+			get;
+		}
+
+		public LoginViewModel(NavigationService<ConfigureConnectionViewModel> navService)
+		{
+			_navService = navService;
+
+			GoToConfigureConnectionCommand = new RelayCommand(GoToConfigureConnection);
+		}
+
+		private void GoToConfigureConnection(object parameter) => _navService.Navigate();
+	}
 }
