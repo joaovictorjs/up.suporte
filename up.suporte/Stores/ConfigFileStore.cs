c1efd8d5ef6b672944fec6b16e312d600ab1aeb8
joaovictorjs <skai7ami@gmail.com>
2024-09-11 17:46:51 -0300

    store to handle app configuration

---
+++ up.suporte/Stores/ConfigFileStore.cs
@@ -0,0 +1,14 @@
+﻿namespace up.suporte.Stores
+{
+    public class ConfigFileStore
+    {
+        private Dictionary<string, string> _currentConfig;
+
+        public Dictionary<string, string> CurrentConfig { get => _currentConfig; set => _currentConfig = value; }
+
+        public ConfigFileStore()
+        {
+            _currentConfig = new Dictionary<string, string>();
+        }
+    }
+}
