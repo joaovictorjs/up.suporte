eb46ce941329956d3180eadd81fba3d6f585d883
joaovictorjs <skai7ami@gmail.com>
2024-09-11 07:57:38 -0300

    login to navigate back to login

--- up.suporte/ViewModels/ConfigureConnectionViewModel.cs
+++ up.suporte/ViewModels/ConfigureConnectionViewModel.cs
@@ -1,6 +1,20 @@
-﻿namespace up.suporte.ViewModels
+﻿using System.Windows.Input;
+using up.suporte.Commands;
+using up.suporte.Services;
+
+namespace up.suporte.ViewModels
 {
-	public class ConfigureConnectionViewModel : BaseViewModel
-	{
-	}
+    public class ConfigureConnectionViewModel : BaseViewModel
+    {
+        private readonly INavigationService _navService;
+        public ICommand NavigateToLoginCommand { get; }
+
+        public ConfigureConnectionViewModel(NavigationService<LoginViewModel> navService)
+        {
+            _navService = navService;
+            NavigateToLoginCommand = new RelayCommand(NavigateToLogin);
+        }
+
+        private void NavigateToLogin(object? parameter) => _navService.Navigate();
+    }
 }
