8975e6fc0c37147bc5b622a35ca74f3c0a4402dd
joaovictorjs <skai7ami@gmail.com>
2024-09-11 23:03:49 -0300

    change DecodeLine access modifier and return type

--- up.suporte/Services/ConfigFileService.cs
+++ up.suporte/Services/ConfigFileService.cs
@@ -3,61 +3,71 @@ using up.suporte.Stores;
 
 namespace up.suporte.Services
 {
-    public interface IConfigFileService
-    {
-        void Read();
-        void Write();
-        bool IsOk();
-    }
+	public interface IConfigFileService
+	{
+		void Read();
+		void Write();
+		bool IsOk();
+	}
 
-    public class ConfigFileService : IConfigFileService
-    {
-        private ConfigFileStore _store;
-        private readonly string _path;
+	public class ConfigFileService : IConfigFileService
+	{
+		private ConfigFileStore _store;
+		private readonly string _path;
 
-        public ConfigFileService(ConfigFileStore store, string path = "up.suporte.ini")
-        {
-            _store = store;
-            _path = path;
-        }
+		public ConfigFileService(ConfigFileStore store, string path = "up.suporte.ini")
+		{
+			_store = store;
+			_path = path;
+		}
 
-        public void Read()
-        {
-            using (StreamReader sr = new StreamReader(_path))
-            {
-                string? line;
-                while ((line = sr.ReadLine()) != null)
-                {
-                    DecodeLine(line);
-                }
-            }
-        }
+		public void Read()
+		{
+			using (StreamReader sr = new StreamReader(_path))
+			{
+				string? line;
+				while ((line = sr.ReadLine()) != null)
+				{
+					(string? key, string? value) pair = DecodeLine(line);
 
-        private void DecodeLine(string line)
-        {
-            string[] keyValue = line.Split('=', 2);
+					if (pair.key == null || pair.value == null)
+					{
+						continue;
+					}
 
-            if (keyValue.Length < 2) return;
+					_store.CurrentConfig.TryAdd(pair.key, pair.value);
+				}
+			}
+		}
 
-            _store.CurrentConfig.TryAdd(keyValue[0].Trim().ToLower(), keyValue[1].Trim().ToLower());
-        }
+		protected (string? key, string? value) DecodeLine(string line)
+		{
+			string[] keyValue = line.Split('=', 2);
 
-        public void Write()
-        {
-            using (StreamWriter sw = new StreamWriter(_path, new FileStreamOptions() { Mode = FileMode.OpenOrCreate }))
-            {
-                sw.WriteLine("address=", _store.CurrentConfig.GetValueOrDefault("address", string.Empty));
-                sw.WriteLine("port=", _store.CurrentConfig.GetValueOrDefault("port", string.Empty));
-                sw.WriteLine("database=", _store.CurrentConfig.GetValueOrDefault("database", string.Empty));
-                sw.WriteLine("username=", _store.CurrentConfig.GetValueOrDefault("username", string.Empty));
-            }
-        }
+			if (keyValue.Length < 2 || string.IsNullOrEmpty(keyValue[0]) || string.IsNullOrEmpty(keyValue[1]))
+			{
+				return (null, null);
+			}
 
-        public bool IsOk()
-        {
-            return _store.CurrentConfig.ContainsKey("address")
-                && _store.CurrentConfig.ContainsKey("port")
-                && _store.CurrentConfig.ContainsKey("database");
-        }
-    }
+			return (keyValue[0].Trim().ToLower(), keyValue[1].Trim());
+		}
+
+		public void Write()
+		{
+			using (StreamWriter sw = new StreamWriter(_path, new FileStreamOptions() { Mode = FileMode.OpenOrCreate }))
+			{
+				sw.WriteLine("address=", _store.CurrentConfig.GetValueOrDefault("address", string.Empty));
+				sw.WriteLine("port=", _store.CurrentConfig.GetValueOrDefault("port", string.Empty));
+				sw.WriteLine("database=", _store.CurrentConfig.GetValueOrDefault("database", string.Empty));
+				sw.WriteLine("username=", _store.CurrentConfig.GetValueOrDefault("username", string.Empty));
+			}
+		}
+
+		public bool IsOk()
+		{
+			return _store.CurrentConfig.ContainsKey("address")
+				&& _store.CurrentConfig.ContainsKey("port")
+				&& _store.CurrentConfig.ContainsKey("database");
+		}
+	}
 }
