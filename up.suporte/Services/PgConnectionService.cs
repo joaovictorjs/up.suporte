c72031070aa0d3c6c5c48306b3056fcec8792b6a
joaovictorjs <skai7ami@gmail.com>
2024-09-12 21:24:44 -0300

    create pgconnection store & service

---
+++ up.suporte/Services/PgConnectionService.cs
@@ -0,0 +1,40 @@
+﻿using Npgsql;
+using up.suporte.Stores;
+
+namespace up.suporte.Services
+{
+    public interface IPgConnectionService
+    {
+        void BuildConnectionString(string address, string port, string database, string username, string password);
+        Task CreateConnection();
+    }
+
+    public class PgConnectionService : IPgConnectionService
+    {
+        private PgConnectionStore _store;
+        private string _connectionString;
+
+        public PgConnectionService(PgConnectionStore store)
+        {
+            _store = store;
+            _connectionString = "";
+        }
+
+        public void BuildConnectionString(string address, string port, string database, string username, string password)
+        {
+            NpgsqlConnectionStringBuilder builder = new NpgsqlConnectionStringBuilder();
+            builder.Host = address;
+            builder.Port = int.Parse(port);
+            builder.Database = database;
+            builder.Username = username;
+            builder.Password = password;
+            _connectionString = builder.ConnectionString;
+        }
+
+        public Task CreateConnection()
+        {
+            _store.CurrentConnection = new NpgsqlConnection(_connectionString);
+            return _store.CurrentConnection.OpenAsync();
+        }
+    }
+}
---
+++ up.suporte/Stores/PgConnectionStore.cs
@@ -0,0 +1,9 @@
+﻿using Npgsql;
+
+namespace up.suporte.Stores
+{
+    public class PgConnectionStore
+    {
+        public NpgsqlConnection? CurrentConnection { get; set; }
+    }
+}
